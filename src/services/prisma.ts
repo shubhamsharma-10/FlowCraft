83ae73c20e0a7f0a511e31e19c37df20a125abc6
Shubham Sharma <shubhamsharma10403@gmail.com>
2025-12-07 00:50:49 +0530

    services

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -7,4 +7,60 @@ const connectionString = config.databaseUrl;
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
 
-export { prisma };
+async function saveProjectToDB({ user_id, name, files }: { user_id: string, name: string, files: Record<string, string> }) {
+    const project = await prisma.project.create({
+        data: {
+            user_id,
+            name
+        }
+    })
+    console.log("files path : ", Object.keys(files));
+    
+    Object.entries(files).forEach(async ([file_path, file_content]) => {
+            await prisma.project_file.create({
+                    data: {
+                        project_id: project.id,
+                        file_path,
+                        file_content
+                    }
+            })
+    })
+
+    return project;
+}
+
+
+async function getProjectFromDB(projectId: string) {
+    const project = await prisma.project.findUnique({
+        where: {
+            id: projectId
+        },
+        include:{
+            project_files: true
+        }
+    })
+
+    if(!project){
+        throw new Error('Project not found');
+    }
+    
+    console.log("Fetched project from DB: ", project);
+    const files: Record<string, string> = {}
+    project.project_files.forEach(({file_path, file_content}) => {
+        files[file_path] = file_content;
+    })
+
+    return {
+        project: {
+            id: project.id,
+            name: project.name,
+            created_at: project.created_at,
+            updated_at: project.updated_at,
+            user_id: project.user_id
+        },
+        files
+    };
+}
+
+// getProjectFromDB("");
+export { prisma, saveProjectToDB, getProjectFromDB };
